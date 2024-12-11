4bbcaedc29ff862c9b0abc6d64b547c51c3474d4
Oleksii Aleferchuk <alex.aleferchuk@gmail.com>
2024-12-11 14:43:10 +0000

    Add MedicationCard model

--- medication/models.py
+++ medication/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+
+DAYS = (("Monday", "Monday"),
+    ("Tuesday", "Tuesday"),
+    ("Wednesday", "Wednesday"),
+    ("Thursday", "Thursday"),
+    ("Friday", "Friday"),
+    ("Saturday", "Saturday"),
+    ("Sunday", "Sunday"))
+
+class MedicationCard(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_medication"
+    )
+    name = models.CharField(max_length=200)
+    days = models.JSONField()
+    time = models.TimeField()
+    doses_per_day = models.PositiveIntegerField(default = 1)
+    photo = CloudinaryField('image', blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        constraints = [
+        models.UniqueConstraint(fields=['user', 'name'], name='unique_medication_for_user')
+        ]
+
+
+    def __str__(self):
+        return f"{self.name} (ID:{self.id})"
+
--- medicine_project/settings.py
+++ medicine_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--6prm55luez-1_f^64+os74bdo4cuw=m+xqy_r&^5ylf!s$@%c'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'medication',
 ]
 
@@ -88,6 +90,11 @@ WSGI_APPLICATION = 'medicine_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
