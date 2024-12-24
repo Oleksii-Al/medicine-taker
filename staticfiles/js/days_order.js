a42052c491b351bf4d371f81e3a81ff0fa6915bd
Oleksii Aleferchuk <alex.aleferchuk@gmail.com>
2024-12-24 12:53:31 +0000

    Collect static

--- staticfiles/js/days_order.js
+++ staticfiles/js/days_order.js
@@ -1,15 +1,25 @@
-let week_order = document.getElementById("toggle-order");
-week_order.innerText = "Monday-Sunday";
-week_order.addEventListener("click", function() {
-    const scheduleContainer = document.querySelector(".schedule-container");
-    const days = Array.from(scheduleContainer.getElementsByClassName("day-schedule"))
+document.addEventListener("DOMContentLoaded", function() {
+    let week_order = document.getElementById("toggle-order");
 
-    days.reverse();
-    days.forEach(day => scheduleContainer.appendChild(day));
-    if (week_order.innerText == "Monday-Sunday") {
-        week_order.innerText = "Sunday-Monday";
-    }    else {
+    if (week_order) {
         week_order.innerText = "Monday-Sunday";
-    }
+        
+        week_order.addEventListener("click", function() {
+            const scheduleContainer = document.querySelector(".schedule-container");
+            const days = Array.from(scheduleContainer.getElementsByClassName("day-schedule"));
 
+            // Переворачиваем порядок дней
+            days.reverse();
+            days.forEach(day => scheduleContainer.appendChild(day));
+
+            // Меняем текст кнопки в зависимости от текущего состояния
+            if (week_order.innerText === "Monday-Sunday") {
+                week_order.innerText = "Sunday-Monday";
+            } else {
+                week_order.innerText = "Monday-Sunday";
+            }
+        });
+    } else {
+        console.log("Element with id 'toggle-order' not found.");
+    }
 });
